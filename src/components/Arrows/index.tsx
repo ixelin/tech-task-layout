57867eab4cdd084099fd67d38c85d6ec58412c16
Nurtay Zhetigenov <nurtay@code-care.pro>
2023-12-24 16:24:50 +0600

    finish project

--- src/components/Arrows/index.tsx
+++ src/components/Arrows/index.tsx
@@ -1,8 +1,9 @@
 import RightArrow from "assets/ArrowRight.svg";
 import LeftArrow from "assets/ArrowLeft.svg";
+import style from "./Arrows.module.css"
 const Arrows = () => {
   return (
-    <div style={{position:'absolute', top:'50%',left:'-5%', margin:'0 auto', width:'110%', display:'flex', justifyContent:'space-between'}}>
+    <div className={style.arrows}>
       <img src={LeftArrow} alt="Left Arrow" style={{padding:'5px'}}/>
       <img src={RightArrow} alt="Right Arrow" style={{padding:'5px'}}/>
     </div>
